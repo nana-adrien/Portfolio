a33e5b595d5d3dfdf6ccea04796370b0ca8828b7
adrien nana <kakonana7@gmail.com>
2026-03-11 05:55:37 +0100

    construction de la structure physique du portfolio

--- composeApp/src/jsMain/kotlin/empire/digiprem/portfolio/app/components/Header.js.kt
+++ composeApp/src/jsMain/kotlin/empire/digiprem/portfolio/app/components/Header.js.kt
@@ -11,14 +11,5 @@ import org.jetbrains.compose.web.dom.Div
 
 @Composable
 actual fun BlurComponent() {
-    Div({
-        style {
-            property("backdrop-filter", "blur(20px)")
-            backgroundColor(rgba(255,255,255,0.2))
-            borderRadius(20.px)
-            padding(20.px)
-        }
-    }) {
-        Text("Glass Card")
-    }
+
 }
--- composeApp/src/wasmJsMain/kotlin/empire/digiprem/portfolio/app/components/Header.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/empire/digiprem/portfolio/app/components/Header.wasmJs.kt
@@ -5,14 +5,5 @@ import androidx.compose.runtime.Composable
 
 @Composable
 actual fun BlurComponent() {
-    Div({
-        style {
-            property("backdrop-filter", "blur(20px)")
-            backgroundColor("rgba(255,255,255,0.2)")
-            borderRadius(20.px)
-            padding(20.px)
-        }
-    }) {
-        Text("Glass Card")
-    }
+
 }
